aa9757f7bd8d5cc609edf09b0f5219fe9c0a1946
zzoman <reumia@gmail.com>
2017-05-22 02:09:42 +0900

    Slick Carousel 적용

--- src/components/partials/Carousel.vue
+++ src/components/partials/Carousel.vue
@@ -1,36 +1,72 @@
 <template>
-  <div>
-    <div class="carousel">
-      <div class="carousel-item">
-        <div class="carousel-body">
-          첫번째 슬라이드
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="carousel-body">
-          두번째 슬라이드
-        </div>
-      </div>
+  <slick class="carousel" ref="slick" :options="slickOptions">
+    <div class="carousel-item">
+      <div class="carousel-body">첫번째 슬라이드</div>
     </div>
-  </div>
+    <div class="carousel-item">
+      <div class="carousel-body">두번째 슬라이드</div>
+    </div>
+  </slick>
 </template>
 
 <script>
+  import Slick from 'vue-slick';
+  import 'slick-carousel/slick/slick.css';
+
   export default {
     name: 'carousel',
+    components: { Slick },
+    data() {
+      return {
+        slickOptions: {
+          arrows: false,
+          dots: true,
+          dotsClass: 'carousel-dots',
+        },
+      };
+    },
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .carousel {
     margin: -20px -20px 20px;
-    background-color: #35a3ff;
     color: #fff;
   }
   .carousel-body {
+    box-sizing: border-box;
     padding: 20px;
     width: 100%;
     height: 200px;
-    box-sizing: border-box;
+    background-color: #35a3ff;
+  }
+  .carousel-dots {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    li ~ li {
+      margin-left: 5px;
+    }
+    .slick-active {
+      button {
+        background-color: #fff;
+      }
+    }
+    button {
+      -webkit-appearance: none;
+      box-sizing: border-box;
+      display: block;
+      margin: 0;
+      padding: 0;
+      width: 8px;
+      height: 8px;
+      border: 2px solid #fff;
+      background-color: transparent;
+      color: transparent;
+    }
   }
 </style>
