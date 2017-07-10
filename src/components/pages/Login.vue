e713a460cd7667f84c01a384526360b7b25f7df1
reumia <reumia@gmail.com>
2017-07-10 16:52:48 +0900

    Login : 준비

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <Page class="page-login">
-    <form class="form-login">
-        <input class="input" type="email" placeholder="이메일"/>
-        <input class="input" type="password" placeholder="비밀번호"/>
+    <form class="form-login" @submit.prevent="login">
+        <input class="input" v-model="email" type="email" placeholder="이메일"/>
+        <input class="input" v-model="password" type="password" placeholder="비밀번호"/>
         <button class="button button-primary" type="submit">로그인</button>
         <button class="button" type="button">회원가입</button>
         <p class="description">비밀번호를 잊어버리셨나요? <a href="/user/find-password">비밀번호 찾기</a></p>
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+  // TODO : 로그인 페이지 구성 https://github.com/jacobwise/vuex-firebase/blob/master/src/components/Login.vue
+  // TODO : 프로그래밍방식 라우터 네비게이션 http://router.vuejs.org/kr/essentials/navigation.htmlhttp://router.vuejs.org/kr/essentials/navigation.html
   import Page from '@/components/partials/Page';
 
   export default {
@@ -18,8 +20,14 @@
     components: { Page },
     data() {
       return {
-        title: '사각사각 소개',
+        email: '',
+        password: '',
       };
+    },
+    methods: {
+      login() {
+        window.console.log(this.email, this.password);
+      },
     },
   };
 </script>
