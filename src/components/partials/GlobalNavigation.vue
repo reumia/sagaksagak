366d3b43e0a16004ba42e64f9fcb2967dd1e587e
reumia <reumia@gmail.com>
2017-09-28 15:59:43 +0900

    ASIDE 유저 정보 UI

--- src/components/partials/GlobalNavigation.vue
+++ src/components/partials/GlobalNavigation.vue
@@ -6,26 +6,27 @@
     </button>
     <!-- 유저 버튼 : 인증 -->
     <div class="nav-user active" v-if="IS_CURRENT_USER_EXIST">
-      <router-link class="link" :to="{ name: 'User', params: { id: currentUser.id } }" >
-        <div class="title">{{ currentUser.name }}</div>
-        <div class="info">
-          <div class="info-email">{{ currentUser.email }}</div>
-        </div>
-      </router-link>
+      <div class="title">{{ currentUser.name }}</div>
+      <div class="info">
+        <div class="info-email">{{ currentUser.email }}</div>
+      </div>
       <div class="button-wrap">
-        <button class="button button-primary button-small" @click="$router.push({ name: 'MyPage' })">
+        <button class="button button-primary button-small" @click="$router.push({ name: 'User', params: { id: currentUser.id } })">
+          <i class="material-icons icon">account_box</i> 내 사각 보러가기
+        </button>
+        <button class="button button-small" @click="$router.push({ name: 'MyPage' })">
           <i class="material-icons icon">settings</i> 정보수정
         </button>
         <button class="button button-danger button-small" @click="$store.dispatch('SIGN_OUT')">
-          <i class="material-icons icon">exit_to_app</i> 로그아웃
+          <i class="material-icons icon">power_settings_new</i> 로그아웃
         </button>
       </div>
     </div>
     <!-- 유저 버튼 : 미인증 -->
     <div class="nav-user" v-else>
-      <router-link class="link" :to="{ name: 'SignIn' }">
-        <div class="title">로그인</div>
-      </router-link>
+      <button class="button button-primary button-small" @click="$router.push({ name: 'SignIn' })">
+        <i class="material-icons icon">power_settings_new</i> 로그인
+      </button>
     </div>
     <!-- 네비게이션 버튼 -->
     <nav class="nav-list">
@@ -84,17 +85,19 @@
   .nav-user {
     overflow: hidden;
     display: block;
+    padding: $space-unit * 2;
     background-color: $color-text;
     &.active {
       background-color: $color-text;
     }
-    .link {
-      display: block;
-      padding: ($space-unit * 1.5) ($space-unit * 2);
-      text-decoration: none;
-    }
     .button-wrap {
-      margin: ($space-unit * 0.5 * -1) ($space-unit * 2) ($space-unit * 1.5);
+      margin-top: $space-unit;
+      .button {
+        text-align: left;
+      }
+      .button ~ .button {
+        margin-top: ($space-unit / 4);
+      }
     }
     .title {
       color: $color-background;
